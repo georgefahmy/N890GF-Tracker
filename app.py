b010401e0b7ae86cb9a4abfbdbe3ebadeafebaea
George Fahmy <geofahm@gmail.com>
2026-05-09 15:56:54 -0700

    requirements

--- app.py
+++ app.py
@@ -36,7 +36,6 @@ from src.fuel_prices import scrape_airnav_to_json
 from src.sw_db_updates import download_dynon_databases_only
 
 CWD_PATH = os.path.abspath(os.getcwd())
-print(CWD_PATH)
 app = Flask(__name__)
 app.secret_key = "827311a9a172036c2f5ebaa0cb68c0ed90b037d30cccf15097627ec1759eee61"
 app.config["MAX_CONTENT_LENGTH"] = 50 * 1024 * 1024
